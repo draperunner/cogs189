220884ee68bf3fcd237521da5bb158e4b881eecc
bweggersen <benjamin.weggersen@gmail.com>
2016-03-02 13:34:12 -0800

    Add error handler in for when Neurosky is not present.

--- server/neurosky.js
+++ server/neurosky.js
@@ -18,6 +18,11 @@ client.on('data', function(data){
 		connected = !connected;
 	}
 });
+
+client.on('error', function(error) {
+	console.log('[Neurosky] Unable to connect: ', error.code);
+})
+
 // initiate connection
 client.connect();
 /** END connect to neurosky **/
